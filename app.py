1e4cab2a25116a8d9b703b09e1f125a4566ea386
SaptakD625 <66028584+Saptak625@users.noreply.github.com>
2023-01-18 16:34:25 +0000

    Add Tailwind.css to project (No Bootstrap and Sass)

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask, render_template, request, flash
-from flask_assets import Environment, Bundle
 import os
 from polygon import RESTClient
 from polygon.rest import models
 from dotenv import load_dotenv
 load_dotenv()
 
-from assets import bundles
 from forms import TickerForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7b7e30111ddc1f8a5b1d80934d336798'
-
-assets = Environment(app)
-assets.register(bundles)
 
 POLYGON_API_KEY = os.getenv('POLYGON_KEY')
 client = RESTClient(POLYGON_API_KEY)
@@ -22,6 +17,11 @@ client = RESTClient(POLYGON_API_KEY)
 def index():
   searchForm = TickerForm()
   return render_template('index.html', data=None, searchForm=searchForm)
+
+@app.route('/tailwind')
+def tailwind():
+  searchForm = TickerForm()
+  return render_template('tailwind_template.html', data=None, searchForm=searchForm)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():

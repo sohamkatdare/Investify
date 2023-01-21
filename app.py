5a4b9fc5ca1b95b84955be7d77c6b5f4610ad97f
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-01-21 17:52:56 -0500

    Added Social Sentiment Analysis, Tweets, and Education Pages

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from forms import TickerForm
+from socialstats import getSocialStats
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7b7e30111ddc1f8a5b1d80934d336798'
@@ -22,17 +23,43 @@ def index():
 def search():
   searchForm = TickerForm()
   data = None
+  tweets = None
+  averageSentiment = None
   if searchForm.ticker.data:
-    print('Validating form...')
     try:
+      ticker = searchForm.ticker.data.upper()
+      tweets, averageSentiment = getSocialStats(ticker)
+      print(tweets)
+      print(averageSentiment)
+      averageSentiment = round(averageSentiment, 2)
       prevAggs = client.get_previous_close_agg(
-          searchForm.ticker.data.upper(),
+          ticker,
           adjusted=True
       )
       data = [prevAggs]
-    except:
+      return render_template('search.html', data=data, searchForm=searchForm, tweets=tweets, averageSentiment=averageSentiment)
+    except Exception as e:
+      print(e)
       flash(f'Ticker "{searchForm.ticker.data.upper()}" not found.', 'error')
-  return render_template('search.html', data=data, searchForm=searchForm)
+  return render_template('search.html', data=data, searchForm=searchForm, tweets=tweets, averageSentiment=averageSentiment)
+
+@app.route('/education')
+def educate():
+  searchForm = TickerForm()
+  return render_template('education.html', data=None, searchForm=searchForm)
+
+@app.route('/education/<path:path>')
+def educatePath(path):
+  searchForm = TickerForm()
+  if path:
+    return render_template(f'education/{path}.html', data=None, searchForm=searchForm)
+  else:
+    return render_template('education.html', data=None, searchForm=searchForm)
+
+@app.errorhandler(404)
+def not_found(e):
+  searchForm = TickerForm()
+  return render_template("404.html", deta=None, searchForm=searchForm)
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ tailwind-watch.bat
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./static/src/css/style.css -o ./static/dist/css/main.css --watch

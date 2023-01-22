5a4d1ecaf1bee21391f821ce9bf4d3fa4434d425
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-01-21 21:24:34 -0500

    Add Tweepy for Vercel Testing

--- socialstats.py
+++ socialstats.py
@@ -1,16 +1,16 @@
 import tweepy
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
-from wordcloud import WordCloud, STOPWORDS
+# import pandas as pd
+# import matplotlib
+# import matplotlib.pyplot as plt
+# from nltk.sentiment import SentimentIntensityAnalyzer
+# import nltk
+# from wordcloud import WordCloud, STOPWORDS
 
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 
 def getSocialStats(ticker):
     # nltk.download('vader_lexicon')
-    plt.style.use('dark_background')
+    # plt.style.use('dark_background')
 
     consumer_key = "44GowZTEx9UJyVAt1Re2qUZTE"
     consumer_secret = "npEIqH5c2lIojIYOCDeSFbk0r0QDM9h4kxkxNe5zqkhY8hs6UE"
@@ -36,48 +36,48 @@ def getSocialStats(ticker):
     # for tweet in own_tweets[:5]:
     #     print(tweet.text)
 
-    df = pd.DataFrame(data=[[tweet.created_at, tweet.text, len(tweet.text), tweet.id, tweet.favorite_count, tweet.retweet_count] for tweet in own_tweets], columns=['Date', 'Tweet', 'Length', 'ID', 'Likes', 'Retweets'])
+    # df = pd.DataFrame(data=[[tweet.created_at, tweet.text, len(tweet.text), tweet.id, tweet.favorite_count, tweet.retweet_count] for tweet in own_tweets], columns=['Date', 'Tweet', 'Length', 'ID', 'Likes', 'Retweets'])
 
-    vader = SentimentIntensityAnalyzer()
-    f = lambda tweet: vader.polarity_scores(tweet)['compound']
-    df['Sentiment'] = df['Tweet'].apply(f)
-    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    # vader = SentimentIntensityAnalyzer()
+    # f = lambda tweet: vader.polarity_scores(tweet)['compound']
+    # df['Sentiment'] = df['Tweet'].apply(f)
+    # df['Date'] = pd.to_datetime(df['Date']).dt.date
 
-    # print(df.head())
-    fig = plt.figure()
-    ax = df['Sentiment'].plot(kind='hist', bins=20, figsize=(5,5), ec='black', color=(30/255, 184/255, 84/255, 0.6))
-    ax.set_facecolor('#171212')
-    ax.set_xlabel('Sentiment')
-    ax.set_ylabel('Frequency')
-    ax.set_title('Sentiment of Tweets (Histogram)')
-    fig.tight_layout()
-    fig.savefig('static/SentimentOfTweets.png', facecolor=plt.gca().get_facecolor())
-    # plt.show()
+    # # print(df.head())
+    # fig = plt.figure()
+    # ax = df['Sentiment'].plot(kind='hist', bins=20, figsize=(5,5), ec='black', color=(30/255, 184/255, 84/255, 0.6))
+    # ax.set_facecolor('#171212')
+    # ax.set_xlabel('Sentiment')
+    # ax.set_ylabel('Frequency')
+    # ax.set_title('Sentiment of Tweets (Histogram)')
+    # fig.tight_layout()
+    # fig.savefig('static/SentimentOfTweets.png', facecolor=plt.gca().get_facecolor())
+    # # plt.show()
 
-    # date_df = df.groupby(['Date']).mean().reset_index()
-    # print(date_df.head())
-    # date_df.plot(kind='line', x='Date', y='Sentiment', ylim=[-1,1])
-    # plt.axhline(y=0, color='black')
-    # plt.ylabel('Average Sentiment')
-    # plt.title('Daily Average Sentiment of Tweets')
+    # # date_df = df.groupby(['Date']).mean().reset_index()
+    # # print(date_df.head())
+    # # date_df.plot(kind='line', x='Date', y='Sentiment', ylim=[-1,1])
+    # # plt.axhline(y=0, color='black')
+    # # plt.ylabel('Average Sentiment')
+    # # plt.title('Daily Average Sentiment of Tweets')
+    # # plt.tight_layout()
+    # # plt.savefig('static/AverageSentiment.png')
+    # # plt.show()
+
+    # text = " ".join(text for text in df.Tweet)
+
+    # stopwords = set(STOPWORDS)
+    # stopwords.update(["HTTPS", "CO", 'T', 'H'])
+
+    # wordcloud = WordCloud(stopwords=stopwords, background_color="#171212").generate(text)
+
+    # plt.imshow(wordcloud, interpolation='bilinear')
+    # plt.axis('off')
     # plt.tight_layout()
-    # plt.savefig('static/AverageSentiment.png')
-    # plt.show()
+    # plt.savefig('static/wordcloud.png', facecolor=plt.gca().get_facecolor())
+    # # plt.show()
 
-    text = " ".join(text for text in df.Tweet)
-
-    stopwords = set(STOPWORDS)
-    stopwords.update(["HTTPS", "CO", 'T', 'H'])
-
-    wordcloud = WordCloud(stopwords=stopwords, background_color="#171212").generate(text)
-
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    plt.tight_layout()
-    plt.savefig('static/wordcloud.png', facecolor=plt.gca().get_facecolor())
-    # plt.show()
-
-    return [i for i in own_tweets if len(i.text.split(' ')) > 15][:5], df.loc[:, 'Sentiment'].mean()
+    return [i for i in own_tweets if len(i.text.split(' ')) > 15][:5], 0#df.loc[:, 'Sentiment'].mean()
 
 if __name__ == '__main__':
     print(getSocialStats('MSFT'))

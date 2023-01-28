279d43dd066bb22f10cc130b0fd07f1046af1d67
neel-kumar-v <79852376+neel-kumar-v@users.noreply.github.com>
2023-01-28 11:58:17 +0000

    commit
    Co-authored-by: Soham Katdare <sohamkatdare@users.noreply.github.com>

--- webscraper.py
+++ webscraper.py
@@ -8,29 +8,57 @@ def investopedia_search(searchquery):
     searchquery = '+'.join(searchquery.split())
 
     url = f'https://investopedia.com/search?q={searchquery}'
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.implicitly_wait(30)
-    driver.get(url)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # driver = webdriver.Chrome(chrome_options=chrome_options)
+    # driver.implicitly_wapython webscraper.pit(30)
+    # driver.get(url)
 
-    anchor = driver.find_element(By.ID, "search-results__link_1-0")
-    link = anchor.get_attribute('href')
-    driver.close()
-    return link
+    # anchor = driver.find_element(By.ID, "search-results__link_1-0")
+    # link = anchor.get_attribute('href')
+    # driver.close()
+    # return link
 
-def investopedia_web_scrape(link):
-    soup = bs(requests.get(link).content, 'html.parser')
-
+def investopedia_web_scrape(searchquery):
     article = ''
-    for o in soup.select('p'):
+
+    searchList = str(searchquery).split()
+    searchquery = ''
+    for i in searchList:
+        searchquery = searchquery + i + '+'
+
+    url = f'https://investopedia.com/search?q={searchquery}'
+    soup = bs(requests.get(url).content, 'html.parser')
+
+    anchor = soup.find(id='search-results__link_1-0')
+    link = anchor['href']
+
+    # driver = webdriver.Firefox()
+    # driver.implicitly_wait(30)
+    # driver.get(url)
+
+    # anchor = driver.find_element_by_id("search=results__link_1-0")
+    # anchor.click()
+    # link = driver.getCurrentUrl()
+
+    soup = bs(requests.get(link).content, 'html.parser')
+    for o in soup.find(id='mntl-sc-page_1-0').find_all('p', recursive=False):
         if len(o.text) > 20:
             article += o.text + ' '
     
     return article
 
-if __name__ == '__main__':
-    # print(investopedia_search('Altman Z Score'))
-    print(investopedia_web_scrape(investopedia_search('Altman Z Score')))
+# if __name__ == '__main__':
+#     # print(investopedia_search('Altman Z Score'))
+#     print(investopedia_web_scrape(investopedia_search('Altman Z Score')))
+
+# print(investopedia_web_scrape('bollinger bands'))
+# print(investopedia_web_scrape('worden stochatics'))
+# print(investopedia_web_scrape('macd'))
+# print(investopedia_web_scrape('On-Balance-volume'))
+# print(investopedia_web_scrape('parabolic stop and reverse'))
+# print(investopedia_web_scrape('relative strength index'))
+# print(investopedia_web_scrape('commodity channel index how to calculate'))
+# print(investopedia_web_scrape('chaikin oscillator'))

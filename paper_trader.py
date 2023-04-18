f19624d661abe1407b5acc6d17db8d3fd73018df
Saptak Das <66028584+Saptak625@users.noreply.github.com>
2023-04-18 00:25:39 -0400

    Finished Basic Python Paper Trading Module

---
+++ paper_trader.py
@@ -0,0 +1,89 @@
+import yfinance as yf
+import pandas as pd
+
+
+class PaperTrader:
+    def __init__(self, portfolio, capital):
+        self.portfolio = portfolio
+        self.capital = capital
+        self.prices = self.get_prices()
+
+    def get_price(ticker):
+        return yf.download([ticker], start="2021-01-01", end="2023-04-16")['Close']
+
+    def get_prices(self):
+        tickers = list(self.portfolio.keys())
+        # Use get_price() to get the price of a single ticker and combine the results into simple pd.DataFrame
+        prices = pd.DataFrame()
+        for ticker in tickers:
+            prices[ticker] = PaperTrader.get_price(ticker)
+        return prices
+
+    def get_portfolio_value(self):
+        portfolio_value = 0
+        for ticker, quantity in self.portfolio.items():
+            price = self.prices[ticker][-1]
+            value = price * quantity
+            portfolio_value += value
+        return portfolio_value + self.capital
+
+    def buy(self, ticker, quantity):
+        prices = PaperTrader.get_price(ticker)
+        price = prices[-1]
+        cost = price * quantity
+        if cost > self.capital: # Check if the purchase can be made (enough free capital (not invested in stocks)).
+            print("Insufficient funds to make this purchase")
+        else:
+            self.portfolio[ticker] = self.portfolio.get(ticker, 0) + quantity
+            self.prices[ticker] = prices
+            print(f"Bought {quantity} shares of {ticker} at ${price}")
+
+    def sell(self, ticker, quantity):
+        if ticker not in self.portfolio:
+            print("You don't own any shares of this stock")
+        elif quantity > self.portfolio[ticker]:
+            print("You don't own enough shares to make this sale")
+        else:
+            price = self.prices[ticker][-1]
+            revenue = price * quantity
+            self.portfolio[ticker] -= quantity
+            if self.portfolio[ticker] == 0:
+                del self.portfolio[ticker]
+                # Remove the ticker from the prices DataFrame
+                self.prices.drop(ticker, axis=1, inplace=True)
+            print(f"Sold {quantity} shares of {ticker} at ${price}")
+            return revenue
+
+    def print_portfolio(self):
+        if not self.portfolio:
+            print("You don't own any shares yet")
+        else:
+            portfolio_df = pd.DataFrame(
+                self.portfolio.items(), columns=['Ticker', 'Quantity'])
+            portfolio_df['Price'] = portfolio_df['Ticker'].map(
+                lambda x: self.prices[x][-1])
+            portfolio_df['Value'] = portfolio_df['Quantity'] * \
+                portfolio_df['Price']
+            portfolio_value = self.get_portfolio_value()
+            print(portfolio_df.to_string(index=False))
+            print(f"\nTotal portfolio value: ${portfolio_value:.2f}")
+
+
+if __name__ == '__main__':
+    # msft = yf.Ticker("MSFT")
+    # msft_history = msft.history(start="2021-01-01", end="2023-04-16")
+    # print(msft_history.head())
+
+    # Create a portfolio with $10,000 to invest
+    portfolio = {'AAPL': 10, 'AMZN': 5, 'TSLA': 3}
+    capital = 10000
+    trader = PaperTrader(portfolio, capital)
+
+    # Buy some shares of a stock
+    trader.buy('MSFT', 2)
+
+    # Sell some shares of a stock
+    trader.sell('AAPL', 5)
+
+    # Print the current portfolio and its total value
+    trader.print_portfolio()

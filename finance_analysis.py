78cf21bc251ad8d0589fd535022c90e3dd39bd54
neel-kumar-v <79852376+neel-kumar-v@users.noreply.github.com>
2023-04-20 20:57:26 +0000

    finance analysis
    Co-authored-by: Suraj Prem Chandran <SurajPrem05@users.noreply.github.com>

--- finance_analysis.py
+++ finance_analysis.py
@@ -7,20 +7,26 @@ def get_pe_and_eps(ticker_symbol):
     return ticker['PE Ratio (TTM)'], ticker['EPS (TTM)']
 
 def get_composite_score(ticker_name):
-    ticker = si(ticker_name)
-    
+    ticker = YahooFinancials(ticker_name)
+
     # Get the financials of the stock
-    income_statement = ticker.get_financial_stmts(frequency = 'quarterly', statement_type = 
-    'income')
-    balance_sheet = ticker.quarterly_balance_sheet
+    latest_income_statement = ticker.get_financial_stmts(frequency='quarterly', statement_type='income')['incomeStatementHistoryQuarterly'][ticker_name][-1]
+    latest_balance_sheet = ticker.get_financial_stmts(frequency='quarterly', statement_type='balance')['balanceSheetHistoryQuarterly'][ticker_name][-1]
+    print(latest_income_statement)
+    print(latest_balance_sheet)
+    income_statement_dict = latest_income_statement[list(latest_income_statement.keys())[0]]
+    balance_sheet_dict = latest_balance_sheet[list(latest_balance_sheet.keys())[0]]
+    
 
-    # Calculate EBIT
-    ebit = income_statement.loc['EBIT'][0]
+    # Calculate EBIT with the latest date possible
+    ebit = income_statement_dict['ebit']
 
-    net_fixed_assets = balance_sheet.loc['Total Non Current Assets'][0]
+    # Calculate Net Fixed Assets
+    net_fixed_assets = balance_sheet_dict['totalNonCurrentAssets']
 
     # Calculate Working Capital
-    working_capital = balance_sheet.loc['Total Capitalization'][0]
+    working_capital = balance_sheet_dict['totalCapitalization']
+
 
     return round(ebit / (net_fixed_assets + working_capital), 4)
 
@@ -30,5 +36,5 @@ def get_news(ticker_name):
 
 if __name__ == '__main__':
     print('Composite Indicator: ', get_composite_score('MSFT'))
-    print('PE and EPS Ratios: ', get_pe_and_eps('MSFT'))
-    print(get_news('MSFT'))
+    # print('PE and EPS Ratios: ', get_pe_and_eps('MSFT'))
+    # print(get_news('MSFT'))

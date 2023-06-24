b3cbaab73655d34b19a991a913cfe1e3ba9d5162
neel-kumar-v <greenworldwins@gmail.com>
2023-06-24 19:46:38 -0400

    conversation styling + navbar responsivity styling

--- static/js/conversation.js
+++ static/js/conversation.js
@@ -158,8 +158,51 @@ async function toggleSave(e) {
     } else {
         removeConversation(conversation_id, messages, request_messages);
     }
-    document.getElementById("save-button").classList.toggle("bg-rose-900");
-    document.getElementById("save-button").classList.toggle("hover:bg-rose-700");
+    saveButton.classList.toggle("bg-rose-900");
+    saveButton.classList.toggle("hover:bg-rose-700");
+    const saveSVG = `
+    <svg class="fill-white ml-2 w-5 transition-all aspect-square" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+    viewBox="0 0 486 486" xml:space="preserve">
+        <g>
+            <g>
+                <path d="M473.7,485.75c6.8,0,12.3-5.5,12.3-12.3v-359.8c0-3.6-1.6-7-4.3-9.3L363,2.85c-0.2-0.2-0.4-0.3-0.6-0.4
+                c-0.3-0.2-0.5-0.4-0.8-0.6c-0.4-0.2-0.7-0.4-1.1-0.6c-0.3-0.1-0.6-0.3-0.9-0.4c-0.4-0.2-0.9-0.3-1.3-0.4c-0.3-0.1-0.6-0.2-0.9-0.2
+                c-0.8-0.1-1.5-0.2-2.3-0.2H12.3C5.5,0.05,0,5.55,0,12.35v461.3c0,6.8,5.5,12.3,12.3,12.3h461.4V485.75z M384.5,461.25h-283v-184.1
+                c0-3.7,3-6.6,6.6-6.6h269.8c3.7,0,6.6,3,6.6,6.6V461.25z M161.8,24.45h180.9v127.8c0,0.8-0.6,1.4-1.4,1.4h-178
+                c-0.8,0-1.4-0.7-1.4-1.4V24.45H161.8z M24.6,24.45h112.8v127.8c0,14.3,11.6,25.9,25.9,25.9h178c14.3,0,25.9-11.6,25.9-25.9V38.75
+                l94.2,80.6v341.9H409v-184.1c0-17.2-14-31.1-31.1-31.1H108.1c-17.2,0-31.1,14-31.1,31.1v184.2H24.6V24.45z"/>
+                <path d="M227.4,77.65h53.8v32.6c0,6.8,5.5,12.3,12.3,12.3s12.3-5.5,12.3-12.3v-44.8c0-6.8-5.5-12.3-12.3-12.3h-66.1
+                c-6.8,0-12.3,5.5-12.3,12.3S220.7,77.65,227.4,77.65z"/>
+                <path d="M304.5,322.85h-123c-6.8,0-12.3,5.5-12.3,12.3s5.5,12.3,12.3,12.3h123c6.8,0,12.3-5.5,12.3-12.3
+                S311.3,322.85,304.5,322.85z"/>
+                <path d="M304.5,387.75h-123c-6.8,0-12.3,5.5-12.3,12.3s5.5,12.3,12.3,12.3h123c6.8,0,12.3-5.5,12.3-12.3
+                S311.3,387.75,304.5,387.75z"/>
+            </g>
+        </g>
+    </svg>`
+    const deleteSVG = `
+    <svg class="fill-white stroke-white ml-2 w-5 aspect-square" enable-background="new 0 0 40 40" id="delete" version="1.1" viewBox="0 0 40 40" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g>
+            <path d="M28,40H11.8c-3.3,0-5.9-2.7-5.9-5.9V16c0-0.6,0.4-1,1-1s1,0.4,1,1v18.1c0,2.2,1.8,3.9,3.9,3.9H28c2.2,0,3.9-1.8,3.9-3.9V16   c0-0.6,0.4-1,1-1s1,0.4,1,1v18.1C33.9,37.3,31.2,40,28,40z"/>
+        </g>
+        <g>
+            <path d="M33.3,4.9h-7.6C25.2,2.1,22.8,0,19.9,0s-5.3,2.1-5.8,4.9H6.5c-2.3,0-4.1,1.8-4.1,4.1S4.2,13,6.5,13h26.9   c2.3,0,4.1-1.8,4.1-4.1S35.6,4.9,33.3,4.9z M19.9,2c1.8,0,3.3,1.2,3.7,2.9h-7.5C16.6,3.2,18.1,2,19.9,2z M33.3,11H6.5   c-1.1,0-2.1-0.9-2.1-2.1c0-1.1,0.9-2.1,2.1-2.1h26.9c1.1,0,2.1,0.9,2.1,2.1C35.4,10.1,34.5,11,33.3,11z"/>
+        </g>
+        <g>
+            <path d="M12.9,35.1c-0.6,0-1-0.4-1-1V17.4c0-0.6,0.4-1,1-1s1,0.4,1,1v16.7C13.9,34.6,13.4,35.1,12.9,35.1z"/>
+        </g>
+        <g>
+            <path d="M26.9,35.1c-0.6,0-1-0.4-1-1V17.4c0-0.6,0.4-1,1-1s1,0.4,1,1v16.7C27.9,34.6,27.4,35.1,26.9,35.1z"/>
+        </g>
+        <g>
+            <path d="M19.9,35.1c-0.6,0-1-0.4-1-1V17.4c0-0.6,0.4-1,1-1s1,0.4,1,1v16.7C20.9,34.6,20.4,35.1,19.9,35.1z"/>
+        </g>
+    </svg>`
+    const word = saveState ? `<span class="max-md:hidden">Unsave</span>` : `<span class="max-md:hidden">Save</span>`;
+    const svg = saveState ? deleteSVG : saveSVG;
+    saveButton.innerHTML = word + svg;
+
+    console.log("Conversation ID: " + conversation_id + ": " + saveState)
 }
 
 
